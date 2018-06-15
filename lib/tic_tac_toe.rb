3c8b67612b6fbe11f132e272051a846327011360
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-15 16:18:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,8 +44,7 @@ def turn(board)
   index = input_to_index(user_input)
   puts "Please enter 1-9:"
   if valid_move?(board,index)
-    move(board,index)
-    display(board)
+    move(board,index) && display(board)
   else turn(board)
   end
 end
